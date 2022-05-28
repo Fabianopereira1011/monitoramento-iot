455947c70a49c2fa0e9d84034cc3cae5f8b993f2
Fabiano Pereira <fabianopereira1011@gmail.com>
2022-05-28 20:57:59 -0300

    fixed array key props

--- src/components/Card.js
+++ src/components/Card.js
@@ -23,27 +23,23 @@ const Color = [
             ]
 
 
-const Card = (props) => {
-    var balance = props.balance
-    //balance = balance * 9.5;
+const Card = ({title, icon, balance}) => {
 
     return (
         <div className={`transform hover:scale-110 cursor-pointer transition delay-100 w-3/12  p-2 py-4 shadow-xl  border rounded-xl bg-gradient-to-r ${Color[props.icon]}`} >
             <div className="flex justify-between">
                 <div></div>
                 <div className=" w-7  h-5 flex items-center justify-center  bg-gray-300 rounded-xl m-1  bg-opacity-30">
-                <AllOutIcon fontSize="small"/>
-                    {arrayIcon[props.icon] }
+                    arrayIcon[{icon}] 
                 </div>
             </div>
             <p className="text-gray-200 text-xs  ">
-                {props.title}
+                {title}
             </p>
             <p className="text-gray-50 text-lg  font-semibold  ">
-                {props.balance} 
+                {balance} 
             </p>
             <p className="text-gray-300  text-sm ">
-                ${balance}
             </p>
 
         </div>
