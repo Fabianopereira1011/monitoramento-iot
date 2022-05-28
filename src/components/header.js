0ae7f1e25679ee830b3b11ff8ae0ccf24bc8c966
Fabiano Pereira <fabianopereira1011@gmail.com>
2022-05-28 09:54:12 -0300

    side bar

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+const Header =() => {
+   return(
+        <div>
+            <h1>bla bla bla...</h1>
+        </div>
+   )
+}
+
+export default Header
---
+++ src/components/sidebar.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import ControlCameraIcon from '@mui/icons-material/ControlCamera';
+
+const Sidebar = () => {
+    return (
+        <div className="md:w-3/12 w-6/12 h-screen shadow-2xl">
+            <div className="flex m-7">
+                <p className="text-4xl">Group</p>
+                <p className="text-4xl italic text-blue-700">4</p>
+                <hr className='bg-blue-900 '/>
+                
+
+            </div>
+            <div className="p-4 space-y-14">
+                <div className="space-y-4" >
+                    <h1 className="text-gray-400">Menu</h1>
+
+                    <div className=''>
+                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-300 hover:text-blue-600  cursor-pointer ">
+                                    <ControlCameraIcon />
+                                    <a href='/sensores/iot' className="text-gray-600  " >Painel</a>
+                        </div>
+                    </div>
+                    
+                    <div className=''>
+                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-300 hover:text-blue-600  cursor-pointer ">
+                                    <ControlCameraIcon />
+                                    <a href='/sensores/iot' className="text-gray-600  " >Sensore</a>
+                        </div>
+                    </div>
+
+                    <div className=''>
+                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-300 hover:text-blue-600  cursor-pointer ">
+                                    <ControlCameraIcon />
+                                    <a href='/sensores/iot' className="text-gray-600  " >Fluviometro</a>
+                        </div>
+                    </div>
+
+
+                    <div className=''>
+                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-300 hover:text-blue-600  cursor-pointer ">
+                                    <ControlCameraIcon />
+                                    <a href='/sensores/iot' className="text-gray-600  " >Desenvolvedores</a>
+                        </div>
+                    </div>
+
+
+                    <div className=''>
+                        <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-300 hover:text-blue-600  cursor-pointer ">
+                                    <ControlCameraIcon />
+                                    <a href='/sensores/iot' className="text-gray-600  " >Painel</a>
+                        </div>
+                    </div>
+
+                </div>
+                
+                
+            </div>
+
+        </div>
+    )
+}
+
+const Acoisa = () =>{
+    return(
+            <div className="flex p-3 text-gray-700  space-x-4 0 hover:bg-gray-50 hover:text-blue-600  cursor-pointer ">
+                                <ControlCameraIcon />
+                                <a href='/sensores/iot' className="text-gray-600  " >Bla bla..</a>
+            </div>
+    )
+}
+
+
+export default Sidebar
