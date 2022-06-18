983ccb0016375ba41b07e2671ea142a462f224b9
Fabiano Pereira <fabianopereira1011@gmail.com>
2022-06-18 19:57:16 -0300

    set env

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,31 +1,34 @@
 import React from 'react'
+//import { withApiAuthRequired } from '@auth0/nextjs-auth0'
 
 import Sidebar from './sidebar'
 import Header from './header'
+import { useUser } from '@auth0/nextjs-auth0'
 
 import Link from 'next/link'
 import LoggingComponent from './loggingComponent'
 
 
 const Painel = (props) => {
-
-  //if (isLoading) return <div>Loading...</div>;
-  //  if (error) return <div>{error.message}</div>;
-  
-  //if(user)
-   
-  return (
-     <div>
-        <div className="flex w-screen h-screen bg-gradient-to-tr from-transparent to to-blue-200">
-          <Sidebar/>
-                <div className='w-screen'>
-                        <Header/>
-                        {props.children}
+      const {user , error , isLoading} = useUser();
+      //import { useUser } from '@auth0/nextjs-auth0'
+      if (isLoading) return <div>Loading...</div>;
+      if (error) return <div>{error.message}</div>;
+      
+      if(user){
+          return (
+            <div>
+                <div className="flex w-screen h-screen bg-gradient-to-tr from-transparent to to-blue-200">
+                  <Sidebar/>
+                        <div className='w-screen'>
+                                <Header/>
+                                {props.children}
+                        </div>
                 </div>
-        </div>
-      </div>
-  )
-
+              </div>
+          )}
+            return <LoggingComponent/>
   }
 
 export default Painel
+//export const getServerSideProps = withApiAuthRequired();
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import Container from '../components/container'
 import LoggingComponent from '../components/loggingComponent';
-import { useUser } from '@auth0/nextjs-auth0'
+
+
 
 
 const Home = () => {
-    const {user , error , isLoading} = useUser();
-
-    //if (isLoading) return <div>Loading...</div>;
-    //  if (error) return <div>{error.message}</div>;
     
-    if(user)
-    return <Container/>
-    return <LoggingComponent/>
+
+        return (
+            <div>
+                <Container/>
+            </div>
+        )
+    
 
 }
 
