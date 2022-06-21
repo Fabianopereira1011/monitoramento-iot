d6d74f0c8746f3911f56a8487e6cda74283e9d6b
Fabiano Pereira <fabianopereira1011@gmail.com>
2022-06-21 10:00:57 -0300

    chart title

--- src/components/Midlle.js
+++ src/components/Midlle.js
@@ -75,7 +75,7 @@ const Middle = () => {
         <div className=" bg-white ml-2   shadow-sm w-8/12 border rounded-xl border-gray-100">
 
             <div className="border-b p-3 border-gray-100">
-                <p className="font-semibold  ">Chuvas mm</p>
+                <p className="font-semibold  ">max-media-min por dia</p>
             </div>
             <div>
                 <Line data={data} />
