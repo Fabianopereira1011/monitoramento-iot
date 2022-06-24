74902885b3d25f70b9083a1eaa536f22df3e1f1d
Fabiano Pereira <fabianopereira1011@gmail.com>
2022-06-24 12:16:58 -0300

    cards set data from api.

--- src/components/container.js
+++ src/components/container.js
@@ -4,27 +4,34 @@ import Card from './CCard'
 import Middle from './Midlle' 
 import Chartline from './chartLine'
 import SensorWater from './sensorWater'
+import { info } from 'autoprefixer'
 
 
 const Container = () => {
-    const [data , setData ] = useState('Conectando. .  .');
+    const [data , setData ] = useState();
+    const [aguaNivel , setAguaNivel]=useState();
+    const [previsaoChuva , setPrevisaoChuva ]=useState();
+    const [fluviometro , setFluviometro ]=useState();
     
 
     const fetchData =async () => {
-            const dataApi = await fetch("https://tcc-group4.herokuapp.com/api/v8/sensors");
+            //const dataApi = await fetch("https://tcc-group4.herokuapp.com/api/v8/sensors");
+            const dataApi = await fetch("http://localhost:8080/api/v8/sensors");
             const dataJson = await dataApi.json();
-            
-            //bellow just for test. 
-            console.log("json format");
-            console.log(dataJson);
-            console.log("O elemento 0 :")
-            console.log(dataJson[0].wateLevel);
-
-            setData(dataJson[0].wateLevel);
-            console.log(data );
+            console.log(data);
+            setData(dataJson);
+            /**/
+            //setData(dataJson[0].wateLevel);
+            /*
+            console.log("dados da api.");
+            console.log(aguaNivel);
+            console.log(previsaoChuva);
+            console.log(fluviometro);
+            */
     }
 
     useEffect(()=>{
+        /**/
         setInterval(() => {
             fetchData();
           }, 2000);
@@ -44,15 +51,37 @@ const Container = () => {
 
             </div>
 
-            <div className="flex-wrap md:flex">
-                <Card title="Monitoramento inteligente" balance={'Aracatuba -> sem chuva'} icon={0}/>
+           { data ?  
+           <div className="flex-wrap md:flex">
+                {/* <Card title="Monitoramento inteligente" balance={data[0].id} icon={0}/>
                 <Card title="Regioes alargadas" balance={'0 ocorrencia'} icon={3}/>
-                <Card title="Fluviometro" balance={'Sem conexao'} icon={2}/>
-            </div>
+                <Card title="Fluviometro" balance={data[1].id} icon={2}/> */}
+                {data.map((element, index) => {			
+                    if(element.id == "fluviometro"){
+                        return <Card key={element.id} title="Fluviometro" balance={element.fluviometro} icon={1}/>;
+                        };
+                })}
+
+                {data.map((element, index) => {			
+                    if(element.id == "previsao_inteligente"){
+                        return <Card key={element.id} title="Prev. intelig." balance={element.chuvaPrevisao} icon={2}/>;
+                        };
+                })}
+
+                <Card title="Regioes alargadas" balance={'0 ocorrencia'} icon={3}/>
+   
+
+            </div> : <h1>Carregando...</h1>}
 
                         {/* flex  ml-3 mt-6 space-x-6  mr-4 */}
             <div className="flex-wrap md:flex">
-                <SensorWater waterLevel={data}/> 
+                {data ?
+                data.map((element, index) => {			
+                    if(element.id == "sensorAgua"){
+                        return <SensorWater key={element.id} waterLevel={element.waterLevel}/>;
+                        };
+                }): ""
+            }
                 <Middle/>                            
             </div>
             <Chartline/>
